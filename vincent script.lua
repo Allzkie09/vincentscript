cee95dcd0a08e1727c6d54c84f5eb590bda10306
Allzkie09 <157404837+Allzkie09@users.noreply.github.com>
2024-07-21 05:39:39 +0800

    Update vincent script.lua

--- vincent script.lua
+++ vincent script.lua
@@ -803,6 +803,42 @@ task.spawn(function()
 
         end
 
+				local a = game:GetService("VirtualInputManager")
+local b = game:GetService("RunService")
+local c = game:GetService("Players").LocalPlayer
+local d = loadstring(game:HttpGet("https://raw.githubusercontent.com/DenDenZYT/DenDenZ-s-Open-Source-Collection/main/Component"))() 
+
+local e = false
+
+spawn(function()
+    b.PreRender:Connect(function()
+        if not getgenv().f then return end
+
+        local g = d.FindTargetBall()
+        if not g then return end
+
+        local h = g.AssemblyLinearVelocity
+        if g:FindFirstChild('zoomies') then 
+            h = g.zoomies.VectorVelocity
+        end
+
+        local i = g.Position
+        local j = c.Character.PrimaryPart.Position
+        local k = (j - i).Unit
+        local l = c:DistanceFromCharacter(i)
+        local m = k:Dot(h.Unit)
+        local n = h.Magnitude
+
+        if m > 0 then
+            local o = l - 5
+            local p = o / n
+
+            if d.IsPlayerTarget(g) and p <= 0.35 and not e then
+                a:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+                wait(0.01)
+                e = true
+            end
+
         task.spawn(function()
 
             if (tick() - aura_table.ball_Warping) >= 0.15 + target_distance_Limited - ball_speed_Limited or ball_Distance <= 10 then
